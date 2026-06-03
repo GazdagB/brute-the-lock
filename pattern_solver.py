0f69b9969b1ad40e17eef7fffc721dbc1dad78ca
Gazdag Balázs <gazdagbal@gmail.com>
2026-06-03 11:32:25 +0200

    add: first working solver

--- pattern_solver.py
+++ pattern_solver.py
@@ -1,5 +1,4 @@
-import random
-
+import time
 REQUIRED_MIDDLE_DOT= {
     (1, 3): 2,
     (3, 1): 2,
@@ -22,54 +21,65 @@ REQUIRED_MIDDLE_DOT= {
 class PatternSolver:
     def __init__(self):
         self.current_pattern = []
-        self.step_limit = 4
+        self.step_limit = 9
+        self.number_of_sequences = 0
         pass
 
-    def validate_move(self, curr_dot, next_dot):
-        if next_dot in self.current_pattern:
+    def validate_move(self, curr_dot, next_dot, pattern):
+        middle_dot = REQUIRED_MIDDLE_DOT.get((curr_dot, next_dot))
+        if next_dot in pattern:
             return False
         elif curr_dot == next_dot:
             return False
-        elif REQUIRED_MIDDLE_DOT.get((curr_dot, next_dot)) is not None:
-            if REQUIRED_MIDDLE_DOT.get((curr_dot, next_dot)) in self.current_pattern:
+        elif middle_dot  is not None:
+            if middle_dot in pattern:
                 return True
             return False
         else:
             return True
 
-    def solve(self):
-
-        if self.isPatternSolved():
-            return self.current_pattern
-
-        if self.current_pattern == []:
-            current_dot = random.randint(1, 9)
-            self.current_pattern.append(current_dot)
-            print(self.current_pattern)
-            return self.current_pattern
-        else:
-            current_dot = self.current_pattern[-1]
 
 
-        valid_next_steps = self.get_valid_moves(current_dot)
-        self.current_pattern.append(random.choice(valid_next_steps))
-        print(self.current_pattern)
-        return None
 
-    def get_valid_moves(self,current_dot):
+    def solve(self,pattern):
+
+        if len(pattern) >= 4:
+            self.number_of_sequences += 1
+            print(pattern)
+        if self.isPatternSolved(pattern):
+            return pattern
+
+        current_dot = pattern[-1]
+        valid_moves = self.get_valid_moves(current_dot, pattern)
+
+        for move in valid_moves:
+            new_pattern = pattern + [move]
+            self.solve(new_pattern)
+
+
+    def get_valid_moves(self,current_dot,pattern):
         all_moves = [1,2,3,4,5,6,7,8,9]
         valid_moves = []
         for move in all_moves:
-            if self.validate_move(current_dot, move):
+            if self.validate_move(current_dot, move, pattern):
                 valid_moves.append(move)
         return valid_moves
 
-    def isPatternSolved(self):
-        return len(self.current_pattern) >= self.step_limit
+    def isPatternSolved(self, pattern):
+        return len(pattern) >= self.step_limit
 
 
 solver = PatternSolver()
-solver.solve()
-solver.solve()
-solver.solve()
-solver.solve()
+start_time = time.perf_counter()
+solver.solve([1])
+solver.solve([2])
+solver.solve([3])
+solver.solve([4])
+solver.solve([5])
+solver.solve([6])
+solver.solve([7])
+solver.solve([8])
+solver.solve([9])
+end_time = time.perf_counter()
+print(solver.number_of_sequences)
+print(f"Ellapsed {round(end_time - start_time,2)} seconds")
