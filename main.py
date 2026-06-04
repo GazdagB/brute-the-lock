40f00fe10f7e6475133373f54961a2ebf2c5116f
GazdagB <gazdagbal@gmail.com>
2026-06-04 23:49:16 +0200

    Move testing to main

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from PatternSolver import PatternSolver
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pattern_solver = PatternSolver()
+start_time = time.perf_counter()
+result = pattern_solver.brute_dfs(4)
+end_time = time.perf_counter()
+print(f"Ellapsed {round(end_time - start_time,2)} seconds")
