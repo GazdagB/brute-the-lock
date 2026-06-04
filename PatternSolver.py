960ce22cd79cc53e1d3fc4074df9cb798b8142c8
GazdagB <gazdagbal@gmail.com>
2026-06-04 23:50:17 +0200

    refactor: improving code quality

--- PatternSolver.py
+++ PatternSolver.py
@@ -1,6 +1,10 @@
 import time
-# Required Middle dots for some steps to as a Tuple Collection
-REQUIRED_MIDDLE_DOT= {
+
+MAX_SEQUENCE_DIGITS = 9
+MIN_SEQUENCE_DIGITS = 4
+ALL_DOTS = [1,2,3,4,5,6,7,8,9]
+
+REQUIRED_MIDDLE_DOT = {
     (1, 3): 2,
     (3, 1): 2,
     (1, 7): 4,
@@ -19,64 +23,69 @@ REQUIRED_MIDDLE_DOT= {
     (6, 4): 5,
 }
 
-
 class PatternSolver:
     def __init__(self):
-        self.step_limit = 9
-        self.number_of_sequences = 0
-        self.bfs_digits = 4
+        self.max_length = MAX_SEQUENCE_DIGITS
         pass
 
-    def validate_move(self, curr_dot, next_dot, pattern):
-        middle_dot = REQUIRED_MIDDLE_DOT.get((curr_dot, next_dot))
+    def is_valid_move(self, curr_dot, next_dot, pattern):
+
         if next_dot in pattern:
             return False
-        elif middle_dot  is not None:
-            if middle_dot in pattern:
-                return True
-            return False
-        else:
+
+        middle_dot = REQUIRED_MIDDLE_DOT.get((curr_dot, next_dot))
+
+        if middle_dot is None:
             return True
 
-    def brute_dfs(self, number_of_digits):
-        for num in range(number_of_digits):
-            self.solve_dfs([num + 1])
+        return middle_dot in pattern
 
-    def solve_dfs(self, pattern):
+    def brute_dfs(self, up_to_length = MAX_SEQUENCE_DIGITS, do_print = False):
+        results = []
+        self.max_length = up_to_length
 
-        if self.is_valid_sequence(pattern):
-            self.number_of_sequences += 1
-            print(pattern)
-        if self.is_max_length(pattern):
-            return pattern
+        def dfs(pattern):
+            if self.is_valid_sequence(pattern):
+                if do_print:
+                    print(pattern)
+                results.append(pattern)
+            if self.is_max_length(pattern):
+                return pattern
 
-        current_dot = pattern[-1]
-        valid_moves = self.get_valid_moves(current_dot, pattern)
+            current_dot = pattern[-1]
+            valid_moves = self.get_valid_moves(current_dot, pattern)
 
-        for move in valid_moves:
-            new_pattern = pattern + [move]
-            self.solve_dfs(new_pattern)
+            for move in valid_moves:
+                new_pattern = pattern + [move]
+                dfs(new_pattern)
+
+        for start_dot in range(1, 10):
+            dfs([start_dot])
+
+        return results
+
 
     def is_valid_sequence(self, pattern):
-        return len(pattern) >= 4
+        return len(pattern) >= MIN_SEQUENCE_DIGITS
 
     def is_max_length(self, pattern):
-        return len(pattern) >= self.step_limit
+        return len(pattern) >= self.max_length
 
-    def handle_pattern(self, pattern):
-        return None
+    def brute_bfs(self, up_to_length = MAX_SEQUENCE_DIGITS, do_print = False):
+        self.max_length = up_to_length
 
-    def brute_bfs(self):
         queue = [[1],[2],[3],[4],[5],[6],[7],[8],[9]]
+        result = []
 
         while len(queue) > 0:
 
             pattern = queue.pop(0)
 
-            if len(pattern) >= 4:
-                self.number_of_sequences += 1
-                print(pattern)
-            if len(pattern) >= self.step_limit:
+            if len(pattern) >= MIN_SEQUENCE_DIGITS:
+                result.append(pattern)
+                if do_print:
+                    print(pattern)
+            if self.is_max_length(pattern):
                 continue
 
             current_dot = pattern[-1]
@@ -86,22 +95,11 @@ class PatternSolver:
                 new_pattern = pattern + [move]
                 queue.append(new_pattern)
 
-
-
+        return result
 
     def get_valid_moves(self,current_dot,pattern):
-        all_moves = [1,2,3,4,5,6,7,8,9]
         valid_moves = []
-        for move in all_moves:
-            if self.validate_move(current_dot, move, pattern):
+        for move in ALL_DOTS:
+            if self.is_valid_move(current_dot, move, pattern):
                 valid_moves.append(move)
         return valid_moves
-
-
-
-solver = PatternSolver()
-start_time = time.perf_counter()
-solver.brute_dfs(9)
-end_time = time.perf_counter()
-print(solver.number_of_sequences)
-print(f"Ellapsed {round(end_time - start_time,2)} seconds")
